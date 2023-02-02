930464c3a34766101e76263cb2329a8b7feb3571
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-02-02 15:07:11 +0000

    Added new audit action enum

---
+++ src/main/java/uk/gov/hmcts/reform/pip/model/enums/AuditAction.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.pip.model.enums;
+
+/**
+ * Enum that contains the different actions that are audited.
+ */
+public enum AuditAction {
+    ATTEMPT_SYSTEM_ADMIN_CREATION,
+    SYSTEM_ADMIN_CREATION,
+    REFERENCE_DATA_UPLOAD,
+    REFERENCE_DATA_DOWNLOAD,
+    VIEW_THIRD_PARTY_USERS,
+    MANAGE_THIRD_PARTY_USER_VIEW,
+    MANAGE_THIRD_PARTY_USER_SUBSCRIPTIONS,
+    USER_MANAGEMENT_VIEW,
+    MANAGE_USER,
+    UPDATE_USER,
+    DELETE_USER,
+    VIEW_BLOB_EXPLORER,
+    BULK_MEDIA_UPLOAD
+}
