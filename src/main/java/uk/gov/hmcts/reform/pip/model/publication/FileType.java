f11ac8740b4559b4da4980d8f9601ca95fee86a2
Kian Kwa <kian.kwa@justice.gov.uk>
2023-05-28 22:18:37 +0100

    Add file extension to file type enum

--- src/main/java/uk/gov/hmcts/reform/pip/model/publication/FileType.java
+++ src/main/java/uk/gov/hmcts/reform/pip/model/publication/FileType.java
@@ -1,9 +1,16 @@
 package uk.gov.hmcts.reform.pip.model.publication;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Enum used in the map of files returned from channel management.
  */
+@Getter
+@AllArgsConstructor
 public enum FileType {
-    EXCEL,
-    PDF
+    EXCEL(".xlsx"),
+    PDF(".pdf");
+
+    private final String extension;
 }
