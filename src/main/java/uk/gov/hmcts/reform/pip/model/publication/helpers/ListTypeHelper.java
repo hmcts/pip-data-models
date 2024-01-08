07b4cf85a71133c37ae5c22a60a57c0678cd2800
Kian Kwa <kian.kwa@justice.gov.uk>
2024-01-08 17:03:05 +0000

    Add OPA results list type

--- src/main/java/uk/gov/hmcts/reform/pip/model/publication/helpers/ListTypeHelper.java
+++ src/main/java/uk/gov/hmcts/reform/pip/model/publication/helpers/ListTypeHelper.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class ListTypeHelper {
-    private static final Set<String> ACRONYMS = Set.of("COP", "ET", "IAC", "SSCS", "SJP");
+    private static final Set<String> ACRONYMS = Set.of("COP", "ET", "IAC", "OPA", "SJP", "SSCS");
     private static final Set<String> CONJUNCTIONS = Set.of("and");
 
     private ListTypeHelper() {
