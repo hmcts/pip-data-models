e3152c1a9a1577dbb3f352afedf3fb097d5d952f
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-11-25 17:40:49 +0000

    Add models for MI reporting

---
+++ src/main/java/uk/gov/hmcts/reform/pip/model/report/LocalSubscriptionMiData.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.pip.model.report;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uk.gov.hmcts.reform.pip.model.subscription.Channel;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LocalSubscriptionMiData {
+
+    private UUID id;
+    private String searchValue;
+    private Channel channel;
+    private String userId;
+    private String locationName;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime createdDate;
+
+    @JsonCreator
+    public LocalSubscriptionMiData(
+        @JsonProperty("id") UUID id,
+        @JsonProperty("searchValue") String searchValue,
+        @JsonProperty("channel") Channel channel,
+        @JsonProperty("userId") String userId,
+        @JsonProperty("locationName") String locationName,
+        @JsonProperty("createdDate") LocalDateTime createdDate
+    ) {
+        this.id = id;
+        this.searchValue = searchValue;
+        this.channel = channel;
+        this.userId = userId;
+        this.locationName = locationName;
+        this.createdDate = createdDate;
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/pip/model/report/PublicationMiData.java
@@ -0,0 +1,71 @@
+package uk.gov.hmcts.reform.pip.model.report;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uk.gov.hmcts.reform.pip.model.publication.ArtefactType;
+import uk.gov.hmcts.reform.pip.model.publication.Language;
+import uk.gov.hmcts.reform.pip.model.publication.ListType;
+import uk.gov.hmcts.reform.pip.model.publication.Sensitivity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@SuppressWarnings("PMD.ExcessiveParameterList")
+@NoArgsConstructor
+public class PublicationMiData {
+
+    private UUID artefactId;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime displayFrom;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime displayTo;
+    private Language language;
+    private String provenance;
+    private Sensitivity sensitivity;
+    private String sourceArtefactId;
+    private Integer supersededCount;
+    private ArtefactType type;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime contentDate;
+    private String locationId;
+    private String locationName;
+    private ListType listType;
+
+    @JsonCreator
+    public PublicationMiData(
+        @JsonProperty("artefactId") UUID artefactId,
+        @JsonProperty("displayFrom") LocalDateTime displayFrom,
+        @JsonProperty("displayTo") LocalDateTime displayTo,
+        @JsonProperty("language") Language language,
+        @JsonProperty("provenance") String provenance,
+        @JsonProperty("sensitivity") Sensitivity sensitivity,
+        @JsonProperty("sourceArtefactId") String sourceArtefactId,
+        @JsonProperty("supersededCount") Integer supersededCount,
+        @JsonProperty("type") ArtefactType type,
+        @JsonProperty("contentDate") LocalDateTime contentDate,
+        @JsonProperty("locationId") String locationId,
+        @JsonProperty("locationName") String locationName,
+        @JsonProperty("listType") ListType listType
+    ) {
+        this.artefactId = artefactId;
+        this.displayFrom = displayFrom;
+        this.displayTo = displayTo;
+        this.language = language;
+        this.provenance = provenance;
+        this.sensitivity = sensitivity;
+        this.sourceArtefactId = sourceArtefactId;
+        this.supersededCount = supersededCount;
+        this.type = type;
+        this.contentDate = contentDate;
+        this.locationId = locationId;
+        this.locationName = locationName;
+        this.listType = listType;
+    }
+
+}
