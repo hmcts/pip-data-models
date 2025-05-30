956020c9017b1b44a3d14971284589cb8b77ef8c
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-05-30 15:37:42 +0100

    Add User Actions

--- src/main/java/uk/gov/hmcts/reform/pip/model/enums/UserActions.java
+++ src/main/java/uk/gov/hmcts/reform/pip/model/enums/UserActions.java
@@ -13,7 +13,10 @@ public enum UserActions {
     UPDATE_LIST_TYPE_LOCATION_SUBSCRIPTION("Update list type for location subscription"),
     CREATE_MEDIA_APPLICATION("Create media application"),
     UPDATE_MEDIA_APPLICATION("Update media application"),
-    DELETE_MEDIA_APPLICATION("Delete media application");
+    DELETE_MEDIA_APPLICATION("Delete media application"),
+    ADD_LOCATION_METADATA("Add location metadata"),
+    DELETE_LOCATION_METADATA("Delete location metadata"),
+    UPDATE_LOCATION_METADATA("Update location metadata");
 
     public final String actionLog;
 
