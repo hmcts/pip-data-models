1a8465da578975cf972f82d7a9644c6db2094c20
kian.kwa <kian.kwa@justice.gov.uk>
2026-02-06 14:45:06 +0000

    Use UUID for user ID

--- src/main/java/uk/gov/hmcts/reform/pip/model/thirdparty/ThirdPartyOauthConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pip/model/thirdparty/ThirdPartyOauthConfiguration.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ThirdPartyOauthConfiguration {
-    private String userId;
+    private UUID userId;
     private String destinationUrl;
     private String tokenUrl;
     private String clientIdKey;
