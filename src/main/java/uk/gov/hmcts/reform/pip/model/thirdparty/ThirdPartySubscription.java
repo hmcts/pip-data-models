2495bb4f40a52eab7fd821e3ecf17fb0c3da28e8
kian.kwa <kian.kwa@justice.gov.uk>
2026-02-17 09:44:04 +0000

    Update healthcheck flag

--- src/main/java/uk/gov/hmcts/reform/pip/model/thirdparty/ThirdPartySubscription.java
+++ src/main/java/uk/gov/hmcts/reform/pip/model/thirdparty/ThirdPartySubscription.java
@@ -16,5 +16,5 @@ public class ThirdPartySubscription {
     List<ThirdPartyOauthConfiguration> thirdPartyOauthConfigurationList;
     UUID publicationId;
     ThirdPartyAction thirdPartyAction;
-    boolean useMockData = false;
+    boolean healthCheck = false;
 }
